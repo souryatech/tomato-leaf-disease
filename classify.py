8f6e43e0c869cddc2abeed16c1c77cf1368162da
SouryaSparks <vzoid8@gmail.com>
2024-11-05 15:50:17 +0900

    classify commit

--- classify.py
+++ classify.py
@@ -4,11 +4,21 @@ from PIL import Image
 import numpy as np
 from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
-from io import BytesIO
+from keras.preprocessing import image
 
-def read_img(image):
+img = 'image.filetype'
 
+MODEL = load_model(os.path.join('models', 'T1'))
 
-img = ''
+classes = ['Early Blight', 'Late Blight', 'Leaf Mold', 'Yellowleaf Curl Virus', 'Mosaic Virus', 'Healthy']
 
-MODEL = load_model()
+image_process = image.load_img(img)
+
+image_process = image.img_to_array(image_process)
+
+image_process = tf.image.resize(image_process, (256,256))
+
+prediction = MODEL.predict(np.expand_dims(image_process, 0))
+
+print(f"Prediction: {classes[np.argmax(prediction)]}")
+print(f"Confidence: {round(np.max(prediction)*100,2)}%")
