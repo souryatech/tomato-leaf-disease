a285940cee6efe040e6fd0736a517086880a13e0
SouryaSparks <vzoid8@gmail.com>
2024-12-25 06:22:40 +0900

    commit

--- classify.py
+++ classify.py
@@ -21,3 +21,12 @@ prediction = MODEL.predict(np.expand_dims(image_process, 0))
 
 print(f"Prediction: {classes[np.argmax(prediction)]}")
 print(f"Confidence: {round(np.max(prediction)*100,2)}%")
+# [tool.uv.index]
+# name = "tensorflow-index"
+# url = "https://storage.googleapis.com/tensorflow/linux"  # Replace with TensorFlow's appropriate URL for your platform if needed.
+
+# [tool.uv.dependencies]
+# tensorflow = {
+#     version = "2.15.0",  # Replace with the desired TensorFlow version, e.g., "2.12.0".
+#     extra = "index"
+# }
